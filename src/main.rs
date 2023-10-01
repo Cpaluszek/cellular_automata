07502fdd095d6d081457592e3fcb8e852a2ffad8
Cpaluszek <paluszek.clement@gmail.com>
2023-10-01 12:47:18 +0200

    Follow tuto

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
         .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Toto".to_string())));
+    commands.spawn((Person, Name("Bob".to_string())));
+    commands.spawn((Person, Name("Mike".to_string())));
+}
+
+// Iterate over every Name compoment for entities that also have a Person component
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}!", name.0);
+    }
 }
 
 fn hello_world() {

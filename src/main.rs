08d40ed46d0bd06b6e4b398f31cd20ca807e8425
Cpaluszek <paluszek.clement@gmail.com>
2023-10-02 15:38:40 +0200

    plugin creation test

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,19 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
+}
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
 }
 
@@ -20,13 +28,14 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Mike".to_string())));
 }
 
-// Iterate over every Name compoment for entities that also have a Person component
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
-    }
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
-fn hello_world() {
-    println!("hello world!");
+// Iterate over every Name compoment for entities that also have a Person component
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
+    }
 }

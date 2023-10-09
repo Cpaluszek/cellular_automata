33a0c28a24404059248c2fd2ffeea7283de9c473
Cpaluszek <paluszek.clement@gmail.com>
2023-10-09 11:07:15 +0200

    spawn camera and draw basic sprite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct Cell;
+
 fn main() {
     App::new()
         .add_plugins(
@@ -15,5 +18,28 @@ fn main() {
                 })
                 .build(),
         )
+        .add_systems(Startup, (spawn_camera, draw_cell))
         .run();
+}
+
+pub fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+pub const CELL_SIZE: f32 = 10.0;
+
+pub fn draw_cell(mut commands: Commands) {
+    let cell_sprite = Sprite {
+        custom_size: Some(Vec2::new(CELL_SIZE, CELL_SIZE)),
+        ..default()
+    };
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            sprite: cell_sprite,
+            ..default()
+        },
+        Cell {},
+    ));
 }

d1782bbd6d727baee42149660b3f2e330b21e49c
Cpaluszek <paluszek.clement@gmail.com>
2023-10-09 15:01:59 +0200

    spawn cell on mouse click

--- src/cell/mod.rs
+++ src/cell/mod.rs
@@ -1,6 +1,11 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 
 pub const CELL_SIZE: f32 = 10.0;
+pub const HALF_CELL_SIZE: f32 = CELL_SIZE / 2.0;
+pub const CELL_SPRITE_SIZE: Vec2 = Vec2::new(CELL_SIZE, CELL_SIZE);
+pub const CELL_COLOR: Color = Color::hsl(10.0, 0.56, 0.91);
+
+// Todo: create parent Cell container
 
 #[derive(Component)]
 pub struct Cell;
@@ -16,13 +21,14 @@ impl Plugin for CellPlugin {
 
 pub fn place_initial_cell(mut commands: Commands) {
     let cell_sprite = Sprite {
-        custom_size: Some(Vec2::new(CELL_SIZE, CELL_SIZE)),
+        custom_size: Some(CELL_SPRITE_SIZE),
+        color: CELL_COLOR,
         ..default()
     };
 
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            transform: Transform::from_xyz(0.0 + HALF_CELL_SIZE, 0.0 + HALF_CELL_SIZE, 0.0),
             sprite: cell_sprite,
             ..default()
         },
@@ -38,14 +44,34 @@ pub fn spawn_cell_with_mouse(
 ) {
     if mouse_button.just_pressed(MouseButton::Left) {
         if let Some(cursor_position) = window_query.single().cursor_position() {
-            println!("Cursor position: {:?}", cursor_position);
             // Convert to world space position
-
             let (camera, camera_transform) = camera_query.single();
             let world_position = camera
                 .viewport_to_world(camera_transform, cursor_position)
                 .map(|ray| ray.origin.truncate());
             println!("World position: {:?}", world_position);
+
+            // Spawn a new cell
+            let mut cell_position = world_position.unwrap();
+            cell_position.x = (cell_position.x / 10.0).round() * 10.0;
+            cell_position.y = (cell_position.y / 10.0).round() * 10.0;
+            println!("Cell position: {:?}", cell_position);
+            commands.spawn((
+                SpriteBundle {
+                    transform: Transform::from_xyz(
+                        cell_position.x + HALF_CELL_SIZE,
+                        cell_position.y + HALF_CELL_SIZE,
+                        0.0,
+                    ),
+                    sprite: Sprite {
+                        custom_size: Some(CELL_SPRITE_SIZE),
+                        color: CELL_COLOR,
+                        ..default()
+                    },
+                    ..default()
+                },
+                Cell {},
+            ));
         }
     }
 }

09238ef8eafb8783bce2c23cef9a5ec370000afe
Cpaluszek <paluszek.clement@gmail.com>
2023-10-09 10:55:02 +0200

    add WindowPlugin configuration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Cellular Automata".into(),
+                        resolution: (1280.0, 720.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .run();
 }

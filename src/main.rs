f0cf07535f9cc911d5db4ad3d1ea4be9caebc063
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-10-10 21:12:30 +0200

    start over

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 use bevy::prelude::*;
 
 pub const CLEAR_COLOR: Color = Color::hsl(240.0, 0.23, 0.09);
-pub const GRID_SIZE: usize = 100;
-
-mod cell;
-mod events;
-mod simulation;
-mod ui;
-
-use cell::CellPlugin;
-use events::*;
-use ui::MainMenuPlugin;
 
 fn main() {
     App::new()
@@ -29,11 +19,8 @@ fn main() {
                 .build(),
         )
         // Events
-        .add_event::<SimulationStart>()
-        .add_event::<SimulationStop>()
-        .add_event::<ExitGame>()
         // Custom Plugins
-        .add_plugins((CellPlugin, MainMenuPlugin))
+        // .add_plugins()
         // Systems
         .add_systems(Startup, spawn_camera)
         .run();
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,8 +1,5 @@
 use bevy::prelude::*;
 
-use crate::events::{ExitGame, SimulationStart, SimulationStop};
-
-// Todo: update color to palette
 const NORMAL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 const PRESSED_COLOR: Color = Color::rgb(0.4, 0.8, 0.8);
 const HOVERED_COLOR: Color = Color::rgb(0.8, 1.0, 1.80);
@@ -23,55 +20,7 @@ pub enum ButtonType {
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_main_menu)
-            .add_systems(Update, button_interaction);
-    }
-}
-
-fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands
-        .spawn((
-            NodeBundle {
-                style: Style {
-                    width: Val::Percent(100.0),
-                    height: Val::Percent(100.0),
-                    ..default()
-                },
-                ..default()
-            },
-            MainMenu {},
-        ))
-        .with_children(|parent| {
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Percent(100.0),
-                        height: Val::Percent(100.0),
-                        align_items: AlignItems::FlexEnd,
-                        ..default()
-                    },
-                    background_color: Color::rgb(0.2, 0.2, 0.2).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent
-                        .spawn((build_classic_button(), ClassicButton(ButtonType::Start)))
-                        .with_children(|parent| {
-                            parent.spawn(build_classic_text("PLAY", &asset_server));
-                        });
-                    parent
-                        .spawn((build_classic_button(), ClassicButton(ButtonType::Stop)))
-                        .with_children(|parent| {
-                            parent.spawn(build_classic_text("STOP", &asset_server));
-                        });
-                    parent
-                        .spawn((build_classic_button(), ClassicButton(ButtonType::Exit)))
-                        .with_children(|parent| {
-                            parent.spawn(build_classic_text("EXIT", &asset_server));
-                        });
-                });
-        });
+    fn build(&self, app: &mut App) {}
 }
 
 fn build_classic_button() -> ButtonBundle {
@@ -105,30 +54,5 @@ fn build_classic_text(value: &str, asset_server: &Res<AssetServer>) -> TextBundl
             ..default()
         },
         ..default()
-    }
-}
-
-fn button_interaction(
-    mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor, &ClassicButton),
-        With<Button>,
-    >,
-    mut start_event_writer: EventWriter<SimulationStart>,
-    mut stop_event_writer: EventWriter<SimulationStop>,
-    mut exit_event_writer: EventWriter<ExitGame>,
-) {
-    for (interaction, mut color, classic_button) in interaction_query.iter_mut() {
-        match *interaction {
-            Interaction::Pressed => {
-                *color = PRESSED_COLOR.into();
-                match classic_button.0 {
-                    ButtonType::Start => start_event_writer.send(SimulationStart),
-                    ButtonType::Stop => stop_event_writer.send(SimulationStop),
-                    ButtonType::Exit => exit_event_writer.send(ExitGame),
-                }
-            }
-            Interaction::Hovered => *color = HOVERED_COLOR.into(),
-            Interaction::None => *color = NORMAL_COLOR.into(),
-        }
     }
 }

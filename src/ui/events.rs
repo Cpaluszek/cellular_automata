fa8020e7dcd8763605784c72c7f43e39ec026e76
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-10-13 22:54:20 +0200

    Create events.rs

---
+++ src/ui/events.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+#[derive(Event)]
+pub struct UiStateChangedEvent(pub UiParameter);
+
+pub enum UiParameter {
+    ResetSimulation,
+    PauseSimulation,
+    // BoardWidth(u32),
+    // BoardHeight(u32),
+    // CycleInterval(u32),
+    // CellColor([f32; 3]),
+    // BackgroundColor([f32; 3]),
+}

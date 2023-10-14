f9428dc3dd4c87741184a29b77f71e91d6502a77
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-10-14 11:00:07 +0200

    use insert_resource instead of init system

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,12 +1,11 @@
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
 
+use crate::{BOARD_SIZE, CELL_COLOR, CLEAR_COLOR, CYCLE_INTERVAL};
+
 use self::{
     resources::UiState,
-    systems::{
-        interaction::handle_ui_interaction,
-        layout::{init_ui_values, ui_panel},
-    },
+    systems::{interaction::handle_ui_interaction, layout::ui_panel},
 };
 
 mod events;
@@ -21,8 +20,13 @@ impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
             .add_event::<UiStateChangedEvent>()
-            .init_resource::<UiState>()
-            .add_systems(Startup, init_ui_values)
+            .insert_resource(UiState {
+                board_width: BOARD_SIZE.0 as u32,
+                board_height: BOARD_SIZE.1 as u32,
+                cycle_interval: CYCLE_INTERVAL.as_millis() as u32,
+                cell_color: [CELL_COLOR.r(), CELL_COLOR.g(), CELL_COLOR.b()],
+                background_color: [CLEAR_COLOR.r(), CLEAR_COLOR.g(), CLEAR_COLOR.b()],
+            })
             .add_systems(Update, (ui_panel, handle_ui_interaction.after(ui_panel)));
     }
 }

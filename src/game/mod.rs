fadca212965ea7ee7788d93f948ecc3ad8717446
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-10-19 22:50:30 +0200

    remove gamePlugin use_cell_map

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -18,37 +18,32 @@ use crate::{
 
 pub struct GameOfLifePlugin {
     pub tick_time_step: Option<f64>,
-    pub use_cell_map: bool,
     pub phantom_c: PhantomData<Moore2dCell>,
     pub phantom_s: PhantomData<ConwayCellState>,
 }
 
 impl Plugin for GameOfLifePlugin {
     fn build(&self, app: &mut App) {
-        if self.use_cell_map {
-            app.add_state::<SimulationState>();
-            if self.use_cell_map {
-                app.insert_resource(CellMap::<Moore2dCell>::default())
-                    .add_systems(Update, handle_new_cells::<Moore2dCell>)
-                    .add_systems(PostUpdate, handle_removed_cells::<Moore2dCell>);
-            }
-            if let Some(time_step) = self.tick_time_step {
-                let duration = Duration::from_secs_f64(time_step);
-                app.add_systems(
-                    Update,
-                    handle_cells::<Moore2dCell, ConwayCellState>
-                        .run_if(on_timer(duration))
-                        .run_if(in_state(SimulationState::Running)),
-                );
-            } else {
-                app.add_systems(
-                    Update,
-                    handle_cells::<Moore2dCell, ConwayCellState>
-                        .run_if(in_state(SimulationState::Running)),
-                );
-            }
-            app.add_systems(Update, systems::coloring::color_sprites::<ConwayCellState>);
+        app.add_state::<SimulationState>();
+        app.insert_resource(CellMap::<Moore2dCell>::default())
+            .add_systems(Update, handle_new_cells::<Moore2dCell>)
+            .add_systems(PostUpdate, handle_removed_cells::<Moore2dCell>);
+        if let Some(time_step) = self.tick_time_step {
+            let duration = Duration::from_secs_f64(time_step);
+            app.add_systems(
+                Update,
+                handle_cells::<Moore2dCell, ConwayCellState>
+                    .run_if(on_timer(duration))
+                    .run_if(in_state(SimulationState::Running)),
+            );
+        } else {
+            app.add_systems(
+                Update,
+                handle_cells::<Moore2dCell, ConwayCellState>
+                    .run_if(in_state(SimulationState::Running)),
+            );
         }
+        app.add_systems(Update, systems::coloring::color_sprites::<ConwayCellState>);
         log::info!("Loaded cellular automata plugin");
     }
 }
@@ -59,7 +54,6 @@ impl GameOfLifePlugin {
     pub const fn new() -> Self {
         Self {
             tick_time_step: Some(CYCLE_INTERVAL),
-            use_cell_map: true,
             // Todo: remove PhantomData
             phantom_c: PhantomData,
             phantom_s: PhantomData,

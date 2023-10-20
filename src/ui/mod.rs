1355008df0bd67d3f839ba7501dc73b48a1e6d62
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-10-20 21:22:34 +0200

    ui interaction in PostUpdate

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -16,7 +16,6 @@ pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
-            // .add_event::<UiStateChangedEvent>()
             .insert_resource(UiSimulationState {
                 simulation_state: true,
                 // cycle_interval: CYCLE_INTERVAL.as_millis() as u32,
@@ -27,6 +26,7 @@ impl Plugin for UIPlugin {
                 board_height: BOARD_SIZE.1 as u32,
                 board_width: BOARD_SIZE.0 as u32,
             })
-            .add_systems(Update, (ui_panel, handle_pause_interaction));
+            .add_systems(Update, ui_panel)
+            .add_systems(PostUpdate, handle_pause_interaction)
     }
 }

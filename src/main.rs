814ede9a4472a758ec559d157b7c5fa969e7dcfd
Cpaluszek <paluszek.clement@gmail.com>
2023-10-20 11:50:53 +0200

    update colors

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,18 @@ use ui::UIPlugin;
 pub const WINDOW_WIDTH: f32 = 1280.0;
 pub const WINDOW_HEIGHT: f32 = 720.0;
 
-pub const CLEAR_COLOR: Color = Color::hsl(240.0, 0.23, 0.09);
-pub const BACKGROND_COLOR: Color = Color::hsl(240.0, 0.21, 0.15);
-pub const CELL_COLOR: Color = Color::hsl(10.0, 0.56, 0.91);
-
 pub const BOARD_SIZE: (i32, i32) = (600, 600);
 pub const SPRITE_SIZE: f32 = 3.0;
 
 pub const CELL_PROBABILITY: f64 = 0.3;
 
 pub const CYCLE_INTERVAL: f64 = 0.1;
+
+// [catppuccin/catppuccin: 😸 Soothing pastel theme for the high-spirited!](https://github.com/catppuccin/catppuccin)
+pub const CLEAR_COLOR: Color = Color::hsl(240.0, 0.23, 0.09);
+pub const BACKGROND_COLOR: Color = Color::hsl(237.0, 0.16, 0.23);
+// pub const BACKGROND_COLOR: Color = Color::hsl(240.0, 0.21, 0.15);
+pub const CELL_COLOR: Color = Color::hsl(10.0, 0.56, 0.91);
 
 fn main() {
     App::new()
